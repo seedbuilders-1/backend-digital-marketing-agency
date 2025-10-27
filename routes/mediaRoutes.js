2c1c49640db6e090b9bf2251c54cc552fb23efdb
seedbuilders-1 <abubakaruabdulkadir@gmail.com>
2025-10-27 14:11:02 +0100

    fixed path

--- routes/mediaRoutes.js
+++ routes/mediaRoutes.js
@@ -1,7 +1,8 @@
 // routes/mediaRoutes.js
 const express = require("express");
 const router = express.Router();
-const auth = require("../middleware/auth"); // Protect the route
+const auth = require("../middlewares/authmiddleware");
+
 const mediaController = require("../controllers/mediaController");
 
 // This is the new endpoint your frontend will call
