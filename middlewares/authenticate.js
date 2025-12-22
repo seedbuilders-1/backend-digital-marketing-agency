02a31d4b0d801f8715e328d8087c80a7946bef89
seedbuilders-1 <abubakaruabdulkadir@gmail.com>
2025-12-22 14:47:00 +0100

    added logs

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,8 +1,12 @@
+const logger = require("../utils/logger");
+
 const authorizeRoles = (...roles) => {
   return (req, res, next) => {
     const userRole = req.user?.role;
     console.log(userRole);
+    logger.info(`User role is ${userRole}`);
     if (!req.user) {
+      logger.error("Unauthorized");
       return res.status(401).json({ message: "Unauthenticated" });
     }
 
